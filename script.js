657047e9487e7b397d075636662b4cd6eab9d92f
Sina <sina13781999@gmail.com>
2023-10-25 17:46:43 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// Data Import
+d3.csv('Albuquerque_Final_2022-06-18.csv').then(data => {
+    // Data processing (convert numeric values if needed)
+    data.forEach(d => {
+      d.value = +d.value; // Ensure 'value' is numeric
+    });
+  
+    // Create the chart after data loading
+    createBarChart(data);
+  });
+
+  // Chart Creation
+  function createBarChart(data) {
+    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+    const width = 800 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
+  
+    const svg = d3.select("#chart")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  
+    const x = d3.scaleBand()
+      .domain(data.map(d => d.category))
+      .range([0, width])
+      .padding(0.1);
+  
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.value)])
+      .nice()
+      .range([height, 0]);
+  
+    svg.selectAll("rect")
+      .data(data)
+      .enter().append("rect")
+      .attr("x", d => x(d.category))
+      .attr("y", d => y(d.value))
+      .attr("width", x.bandwidth())
+      .attr("height", d => height - y(d.value))
+      .attr("fill", "steelblue");
+  
+    svg.append("g")
+      .attr("class", "x-axis")
+      .attr("transform", `translate(0,${height})`)
+      .call(d3.axisBottom(x));
+  
+    svg.append("g")
+      .attr("class", "y-axis")
+      .call(d3.axisLeft(y));
+  
+    // Add labels, titles, and other interactive features as needed
+  }
+  
